438144ceaf5b55e271e332e5c4d83c5797fdef3d
Ghazaleh Bakhtiariazad <ghazal.b93@gmail.com>
2023-02-07 11:25:08 -0800

    Add files

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace TCP_Server
+{
+    class Program
+    {
+
+    }
+}
---
+++ Serverr.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TCP_Server
+{
+    class Server
+    {
+        const int PORT_NO = 5000;
+        const string SERVER_IP = "10.0.0.93";
+        const string message = "Hello!";
+
+        static void Main(string[] args)
+        {
+            //---listen at the specified IP and port no.---
+            IPAddress localAdd = IPAddress.Parse(SERVER_IP);
+            TcpListener listener = new TcpListener(localAdd, PORT_NO);
+            Console.WriteLine("Listening...");
+            listener.Start();
+
+            //---incoming client connected---
+            TcpClient client = listener.AcceptTcpClient();
+
+            //---get the incoming data through a network stream---
+            NetworkStream nwStream = client.GetStream();
+            byte[] buffer = new byte[client.ReceiveBufferSize];
+
+            while (true)
+            {
+                //---read incoming stream---
+                int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize);
+
+                //---convert the data received into a string---
+                string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                Console.WriteLine("Received : " + dataReceived);
+
+                if (dataReceived == "1")
+                {
+                    //---write back the text to the client---
+                    byte[] messageBytes = System.Text.Encoding.ASCII.GetBytes(message);
+                    Console.WriteLine("Sending back : " + dataReceived);
+                    nwStream.Write(messageBytes, 0, messageBytes.Length);
+                    Console.WriteLine("Data has been sent");
+                }
+            }
+            client.Close();
+            listener.Stop();
+            Console.ReadLine();
+        }
+    }
+}
+
